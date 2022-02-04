6f456371b6d19d176a137f21ce4ddef821130d99
Michelle Tay <mich.tay.elle@gmail.com>
2022-02-04 19:49:15 +0800

    2nd Commit
    Mock Api

--- src/API/API.js
+++ src/API/API.js
@@ -1,59 +1,17 @@
 // import axios from 'axios';
 
 // export const API = axios.create({
-//     baseURL:'https://api.data.gov.sg/v1/transport/carpark-availability',
+//     baseURL:'http://datamall2.mytransport.sg',
 //     headers: {
-//       'AccountKey': '139a3035-e624-4f56-b63f-89ae28d4ae4c'
+//       'AccountKey': 'Id2T89xUQH+qBlNyK2x7tQ==',
+//       'accept': 'application/json'
 //     }    
 //   });
 
-export const API = {
-    "odata.metadata": "http://datamall2.mytransport.sg/ltaodataservice/$metadata#CarParkAvailability",
-    "value": [
-        {
-            "CarParkID": "1",
-            "Area": "Marina",
-            "Development": "Suntec City",
-            "Location": "1.29375 103.85718",
-            "AvailableLots": 1104,
-            "LotType": "C",
-            "Agency": "LTA"
-        },
-        {
-            "CarParkID": "2",
-            "Area": "Marina",
-            "Development": "Marina Square",
-            "Location": "1.29115 103.85728",
-            "AvailableLots": 1091,
-            "LotType": "C",
-            "Agency": "LTA"
-        },
-        {
-            "CarParkID": "3",
-            "Area": "Marina",
-            "Development": "Raffles City",
-            "Location": "1.29382 103.85319",
-            "AvailableLots": 453,
-            "LotType": "C",
-            "Agency": "LTA"
-        },
-        {
-            "CarParkID": "4",
-            "Area": "Marina",
-            "Development": "The Esplanade",
-            "Location": "1.29011 103.85561",
-            "AvailableLots": 448,
-            "LotType": "C",
-            "Agency": "LTA"
-        },
-        {
-            "CarParkID": "5",
-            "Area": "Marina",
-            "Development": "Millenia Singapore",
-            "Location": "1.29251 103.86009",
-            "AvailableLots": 532,
-            "LotType": "C",
-            "Agency": "LTA"
-        }
-    ]
-};
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL:'http://localhost:3000'
+});
+
+export default API;
--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import './App.css';
 import LotAvailability from './components/LotAvailability';
-import {API} from './API/API';
-import {useState, useEffect} from 'react'
-
+import API from './API/API.js';
+import React, {useState, useEffect} from 'react';
 
 
 function App() {
 
   const [lotData, setLotData] = useState([]);
 
-  const getData = async (lotData) => {
-    const {status, data} = await API.get(`https://api.data.gov.sg/v1/transport/carpark-availability${lotData}`);
+  const getData = async () => {
+    const {status, data} = await API.get('/data');
     if (status === 200) {
+      setLotData(data);
       console.log('Carpark lot data:', data);
-      const d = data[0];
-      const tempObj = {
-        CarParkID: d.CarParkID,
-        Area: d.Area,
-        Location: d.Location,
-        AvailableLots: d.AvailableLots
-      };
-      setLotData(tempObj);
-    } else {
-      console.log('Error');
     };
   };
+  
+
+  // const getData = async (lotData) => {
+  //   const {status, data} = await API.get(`/ltaodataservice/CarParkAvailabilityv2?`);
+  //   if (status === 200) {
+  //     console.log('Carpark lot data:', data);
+  //     const d = data[0];
+  //     const tempObj = {
+  //       CarParkID: d.CarParkID,
+  //       Area: d.Area,
+  //       Location: d.Location,
+  //       AvailableLots: d.AvailableLots
+  //     };
+  //     setLotData(tempObj);
+  //   } else {
+  //     console.log('Error');
+  //   };
+  // };
 
   useEffect( () => {
     console.log('App.useEffect mounted')
-    if (lotData)
-      getData(lotData)
-  },[lotData]);
+    getData();
+  },[]);
 
   return (
     <>
