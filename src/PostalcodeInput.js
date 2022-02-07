132108e0bc5e7268937f32f818fefa4211d19895
normanhfz <normanhfz@gmail.com>
2022-02-07 15:27:56 +0800

    Update PostalcodeInput.js

--- src/PostalcodeInput.js
+++ src/PostalcodeInput.js
@@ -1,65 +1,12 @@
-import axios from "axios";
-import {useState} from "react";
-
-function PostalcodeInput({props}) {
-
-  const{setUserLoc} = props;  
-  const [postalCode, setPostalCode] = useState("");
-    
-
-  const handlePostalCode = (e)=>{
-    console.log(e.target.name,e.target.value);
-    setPostalCode(e.target.value);
-  }
-
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    geocode(postalCode)
-    setPostalCode("")
-  }
 
 
-
-  function geocode(){
-    
-    const location = postalCode;
-    axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
-      params:{
-        address:location,
-        key:'AIzaSyDJhmmjCCsw1qzIT6hQ7M6YSnOx3ZvRpU8'
-      }
-    })
-    .then(function(response){
-      //log full response
-      console.log(response);
-      //log long and lat
-      setUserLoc(response.data.results[0].geometry.location)
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-  }
-
-  // console.log(setUserLoc);
-
-  console.log('Postal Code:', postalCode);
+function PostalCodeInput() {
   return (
     <div className="postalcode-input">
-      <h1>postal code input</h1>
-      <div className="form-section">
-        <form>
-          <input
-            type="text"
-            placeholder="Park Where?"
-            name="postal-code"
-            onChange={handlePostalCode}
-            value={postalCode}
-          ></input>
-          <button onClick={handleSubmit}>Submit</button>
-        </form>
-      </div>
+      <h1>postal code</h1>
+     
     </div>
   );
 }
 
-export default PostalcodeInput;
+export default PostalCodeInput;
