9e6d9dee019e8f7279b7a79278a9aabc434aa4ed
janiceyap <janice.sl.yap@gmail.com>
2022-02-08 11:32:03 +0800

    Some CSS Updates

--- src/components/AddFav.js
+++ src/components/AddFav.js
@@ -1,10 +1,22 @@
 //import { IoIosHeartDislike, IoIosHeartEmpty } from "react-icons/io";
+import {useState} from 'react';
 
 function AddFav (props) {
 
     const {favoriteCarpark, setFavoriteCarpark, carpark} = props;
+    const carparkInFav = favoriteCarpark.find((data) => data.CarParkID === carpark.CarParkID);
+    let iniFav;
+    if (carparkInFav){
+        iniFav = true;
+    } else {
+        iniFav = false;
+    }
+
+    const [isFav, setIsFav] = useState(iniFav);
+    // isFav returns an object when "true", and if carpark is not in favoriteCarpark array, it returns undefined.
 
     const addFavCarpark = () => {
+        setIsFav(!isFav);
         favoriteCarpark.push(carpark)
         setFavoriteCarpark(favoriteCarpark);
         console.log("favouriteCarpark",favoriteCarpark);
@@ -12,6 +24,7 @@ function AddFav (props) {
     };
 
     const deleteFavCarpark = () => {
+        setIsFav(!isFav);
         setFavoriteCarpark(favoriteCarpark.filter(item => {
             console.log(item.CarParkID);
             console.log(carpark.CarParkID);
@@ -20,10 +33,12 @@ function AddFav (props) {
         console.log("deleted from Favourite");
     };
 
+
+    // here we do the conditional rendering
     return (
         <>
-            <button onClick={addFavCarpark} style={{color: "red"}}> ♥ </button>
-            <button onClick={deleteFavCarpark} style={{color: "red"}}> ⛔ </button>
+            {isFav ? <div className="heart" onClick={deleteFavCarpark} style={{color: "red"}}> ♥ </div> :
+            <div className="heart" onClick={addFavCarpark} style={{color: "red"}}> ♡ </div>}
         </>
     )
 }
